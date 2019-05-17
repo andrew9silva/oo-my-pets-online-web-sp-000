cd99720ab385d2209ba3de878e91b0ec9077565e
Andrew Silva <andrewsilva9@gmail.com>
2019-05-17 01:38:34 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,4 +2,10 @@ class Owner
   @@all = []
   attr_reader :species
   attr_accessor :name, :pets
+
+  def initialize(species)
+    @species = species
+    @@all << self
+    @pets = {:fishes => [], :cats => [], :dogs => []}
+    
 end
